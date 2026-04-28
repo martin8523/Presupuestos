e22d7f7c1f5e987c51242a84410ac50526a80891
martin8523 <110629999+martin8523@users.noreply.github.com>
2026-04-28 10:04:23 -0300

    Update presupuesto.js

--- presupuesto.js
+++ presupuesto.js
@@ -11,6 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     { descripcion: "Nicho Nuevo", precio: 950000 },
     { descripcion: "Nicho Usado", precio: 480000 },
     { descripcion: "Cremacion", precio: 1020000 },
+	{ descripcion: "Servicio Velacion 8 Hs ", precio: 1268491 },
     { descripcion: "Hora de Velación", precio: 160000 },
     { descripcion: "Gastos Administrativos", precio: 182000 },
 	{ descripcion: "Traslado por Kilometro", precio: 5200 },
